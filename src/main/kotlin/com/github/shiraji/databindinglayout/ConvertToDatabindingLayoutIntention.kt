1478acfa865e7a9e3570557e9fee51e7ba573112
shiraji <isogai.shiraji@gmail.com>
2016-11-20 22:19:01 +0900

    Make it happens

--- src/main/kotlin/com/github/shiraji/databindinglayout/ConvertToDatabindingLayoutIntention.kt
+++ src/main/kotlin/com/github/shiraji/databindinglayout/ConvertToDatabindingLayoutIntention.kt
@@ -1,9 +1,11 @@
 package com.github.shiraji.databindinglayout
 
 import com.intellij.codeInsight.intention.IntentionAction
+import com.intellij.lang.xml.XMLLanguage
 import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.project.Project
 import com.intellij.psi.PsiFile
+import com.intellij.psi.XmlElementFactory
 import com.intellij.psi.xml.XmlFile
 
 class ConvertToDatabindingLayoutIntention : IntentionAction {
@@ -15,9 +17,21 @@ class ConvertToDatabindingLayoutIntention : IntentionAction {
         if (file !is XmlFile) return false
         val rootTag = file.rootTag ?: return false
         return rootTag.name != "layout"
-                && rootTag.getAttribute("xmlns:android") == null
+                && rootTag.getAttribute("xmlns:android") != null
     }
 
     override fun invoke(project: Project, editor: Editor?, file: PsiFile?) {
+        if (file !is XmlFile) return
+        val rootTag = file.rootTag ?: return
+        val xmlnsAttribute = rootTag.attributes
+                .filter { it.name.startsWith("xmlns:") }
+        val attributeText = xmlnsAttribute
+                .map { it.text }
+                .joinToString(separator = " ")
+        val factory = XmlElementFactory.getInstance(project)
+
+        xmlnsAttribute.forEach { it.delete() }
+        val layoutXmlTag = factory.createTagFromText("<layout $attributeText>${rootTag.text}</layout>", XMLLanguage.INSTANCE)
+        rootTag.replace(layoutXmlTag)
     }
 }

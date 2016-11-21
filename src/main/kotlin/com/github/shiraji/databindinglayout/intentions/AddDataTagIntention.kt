7341ff5bb5b904b074a9341cad800d8a4c18bd77
shiraji <isogai.shiraji@gmail.com>
2016-11-22 00:08:01 +0900

    Add <data> tag Intention. Close #4

---
+++ src/main/kotlin/com/github/shiraji/databindinglayout/intentions/AddDataTagIntention.kt
@@ -0,0 +1,28 @@
+package com.github.shiraji.databindinglayout.intentions
+
+import com.intellij.codeInsight.intention.IntentionAction
+import com.intellij.lang.xml.XMLLanguage
+import com.intellij.openapi.editor.Editor
+import com.intellij.openapi.project.Project
+import com.intellij.psi.PsiFile
+import com.intellij.psi.XmlElementFactory
+import com.intellij.psi.xml.XmlFile
+
+class AddDataTagIntention : IntentionAction {
+    override fun getFamilyName() = "Add <data> tag"
+    override fun getText() = "Add <data> tag"
+
+    override fun startInWriteAction() = true
+
+    override fun isAvailable(project: Project, editor: Editor?, file: PsiFile?): Boolean {
+        if (file !is XmlFile) return false
+        val rootTag = file.rootTag ?: return false
+        return rootTag.name == "layout" && rootTag.findSubTags("data").isEmpty()
+    }
+
+    override fun invoke(project: Project, editor: Editor?, file: PsiFile?) {
+        if (file !is XmlFile) return
+        val newTag = file.rootTag?.addSubTag(XmlElementFactory.getInstance(project).createTagFromText("<data></data>", XMLLanguage.INSTANCE), true)
+        newTag?.value?.textRange?.startOffset?.let { editor?.caretModel?.moveToOffset(it) }
+    }
+}
